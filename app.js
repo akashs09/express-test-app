dc9655053ed49a80cafdd164516017e922ae18f0
Akash Sharma <sharma.akash028@gmail.com>
2017-12-14 10:46:27 -0500

    second stage flashcard express app

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/cards', (req, res) => {
+    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is." });
+});
+
+app.get('/sandbox', (req,res) => {
+    res.render('sandbox',{firstName, lastName});
+});
+
+const firstName = ["Will","Leo","James","Paula","Larry"];
+const lastName = ["Smith","Di Caprio","Franco","Patton","David"];
+
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
